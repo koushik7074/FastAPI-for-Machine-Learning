9405f6abff083587b35534f41f6f9b659f725ace
koushik <bkoushik81@gmail.com>
2026-01-10 10:35:52 +0530

    Added first pydantic model for data validation

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name:str
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'message': 'Hello FastAPI!'}
+
+
+@app.get('/user', response_model=User)
+def get_user():
+    return User(id=1, name='Jon')
